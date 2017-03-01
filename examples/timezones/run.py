4697565a6c389edab6581d8bf5d683ebcc379596
Roy Hyunjin Han <rhh@crosscompute.com>
2017-03-01 14:51:07 +0000

    Add revised timezones example

---
+++ examples/timezones/run.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+selected_timezones = argv[1].splitlines()
+print('selected_timezones = %s' % ', '.join(selected_timezones))
+print('selected_timezone_count = %s' % len(selected_timezones))
