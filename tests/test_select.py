901b1ff2abcd09b1ea200a68546b6360978b52a4
Roy Hyunjin Han <rhh@crosscompute.com>
2017-10-24 13:01:53 -0400

    Fix tests

--- tests/test_select.py
+++ tests/test_select.py
@@ -1,4 +1,6 @@
+from crosscompute.exceptions import DataTypeError
 from crosscompute_select import SelectType
+from pytest import raises
 
 
 X = SelectType
@@ -7,7 +9,8 @@ DEFAULT_VALUE = ['one', 'two'], []
 
 def test_parse():
     assert X.parse('one', DEFAULT_VALUE) == (['one', 'two'], ['one'])
-    assert X.parse('one\nthree', DEFAULT_VALUE) == (['one', 'two'], ['one'])
+    with raises(DataTypeError):
+        X.parse('one\nthree', DEFAULT_VALUE)
 
 
 def test_render():
